9874d21460f21779733f7a86d3e6cbb37a5f7645
Mark Spannbauer <msmarkieer@gmail.com>
2020-07-25 18:11:46 -0500

    You can now mark things as completed or incomplete

--- Windows/mainWindow/mainWindow.js
+++ Windows/mainWindow/mainWindow.js
@@ -10,16 +10,38 @@ let selectedItem;
 
 function setupTrigger() {
     const triggers = document.querySelectorAll(".sidenav-trigger");
-    console.log(triggers)
     Array.from(triggers).forEach(function (event) {
-      event.addEventListener("click", function (event) {
-          const dataset = event.toElement.dataset;
+      if (!event.getAttribute("enabled")) {
+        event.addEventListener("click", function (e) {
+          const dataset = e.toElement.dataset;
           header.textContent = dataset.title;
           subheader.textContent = dataset.importance;
           description.textContent = dataset.description;
 
-          selectedItem = event.target.parentElement.parentElement;
+          selectedItem = e.target.parentElement.parentElement;
         });
+        event.setAttribute("enabled", "true");
+      }
+    });
+
+    const checkboxes = document.querySelectorAll(".checkbox");
+    Array.from(checkboxes).forEach(function (event) {
+      if (!event.getAttribute("enabled")) {
+        event.addEventListener("click", function (e) {
+          const data = e.toElement.dataset;
+          const search = `span-${data.title}`
+          const span = document.getElementById(search);
+          if (e.target.checked) {
+            e.toElement.parentElement.children[1].textContent = 'Completed';
+            span.classList.add("completed");
+          } else {
+            e.toElement.parentElement.children[1].textContent = "Incomplete";
+            span.classList.remove("completed");
+            
+          }
+        });
+        event.setAttribute("enabled", "true");
+      }
     });
     
 }
@@ -33,11 +55,11 @@ ipcRenderer.on("item:add", function (e, item) {
   tr.innerHTML = `
                 <td>
                     <label>
-                        <input type="checkbox" class="filled-in" />
+                        <input type="checkbox" class="filled-in checkbox" data-title="${item.title}"/>
                         <span>Incomplete</span>
                     </label>
                 </td>
-                <td>${item.title}</td>
+                <td><span id="span-${item.title}">${item.title}</span></td>
                 <td>${item.importance}</td>
                 <td>
                     <a 
